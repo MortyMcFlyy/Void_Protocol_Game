5c696bd898140449e8115aa88575ac9947f8a357
Felix Lukowski <felix.lukowski@web.de>
2025-09-06 15:50:39 +0200

    changes

--- Assets/Scripts/CameraLookAt.cs
+++ Assets/Scripts/CameraLookAt.cs
@@ -7,6 +7,11 @@ public class LookAtPlayer : MonoBehaviour
     void LateUpdate()
     {
         if (!player) return;
-        transform.LookAt(player);
+
+        float fixedY = transform.position.y; // Y-Position merken
+        if (Vector3.Distance(transform.position, player.position) > 0.01f)
+            transform.LookAt(player, Vector3.up);
+        // Y-Position zurücksetzen
+        transform.position = new Vector3(transform.position.x, fixedY, transform.position.z);
     }
 }
